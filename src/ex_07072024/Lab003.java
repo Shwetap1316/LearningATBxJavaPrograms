30bf1f2db59e496c213a1c86351887ed013b49e2
Shweta <shwetapatil1316@gmail.com>
2024-08-08 16:35:58 +0530

    Lab exercise of 7 July 2024

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,14 @@
+package ex_07072024;
+
+public class Lab003 {
+
+
+    public static void main(String[] args) {
+
+        //Single comment - This code will not be executed or run
+
+        /*
+              Multiline comments used for Documentation
+         */
+    }
+}
