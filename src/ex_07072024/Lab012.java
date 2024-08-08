31f3efdaa87af3dd8fd49619add2021e54677b52
Shweta <shwetapatil1316@gmail.com>
2024-08-08 17:55:47 +0530

    Lab exercise of 7 July 2024

---
+++ src/ex_07072024/Lab012.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab012 {
+
+    public static void main(String[] a) {
+        System.out.println("Interview Question");
+
+        //Ques:Is it a valid code
+        //Answer: Yes it is a valid code public static void main(String[] a -- it can be any name)
+    }
+}
